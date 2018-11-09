7468be07218bffa65e9dd3b5e3c4a3fa292fd71b
vivideljo <vivideljo@gmail.com>
2018-11-09 17:51:11 +0900

    Add views to create/edit board post inside the site.

--- myboard/settings.py
+++ myboard/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
@@ -112,7 +112,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False # local time 쓰려고
 
 
 # Static files (CSS, JavaScript, Images)
